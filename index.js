565e5c4282239bdaa29ab4443fb4f5ebd625b1a3
Shamika02 <71770711+Shamika02@users.noreply.github.com>
2020-11-28 20:46:57 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var i= 5;
+if (i > 2)
+console.log("I am greater than 5");
+console.log("I am Not greater than 5");
+
+var i= 6;
+if (i > 3)
+console.log ("Is the length");
+
+
+var i= cat;
+if (i= cat){
+console.log  ("I am not a cat");
+}
+
+  else {
+    console.log  ("I am not a cat but a dog");
+  }
